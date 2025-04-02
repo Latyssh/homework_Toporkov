7ffb798d6c837836c38b11898f25d01ccd0b8d99
Latyssh <leha.toporkov.19@mail.ru>
2025-04-02 20:30:19 +0500

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class CubeJump : MonoBehaviour
+{
+    [SerializeField] private float jumpForce = 5f; // Сила прыжка
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
